dfe3d6f753eda12f63e95b252772bac19e9372f1
NTH <tang0929@naver.com>
2024-05-24 11:19:02 +0900

    20240524

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,6 +10,11 @@ import ClassComponent from './components/R01_ClassComponent'
 
 import FunctionComponent from './components/R02_FunctionComponent';
 
+import Props1 from './components/R03_Props1';
+import Props2 from './components/R04_Props2';
+import Props3 from './components/R05_Props3';
+import StateEx1 from './components/R06_State1';
+import StateEx2 from './components/R07_State2';
 
 function App() {
 
@@ -22,17 +27,28 @@ function App() {
     /* fragment(<> </>) : 반환되는 요소(컴포넌트)를 묶는 용도, 해석 X */
     <>
       {/* ClassComponent를 현재 위치에 렌더링 */}
-      <ClassComponent/> 
-      
-        
-      <ClassComponent/> 
-        
-        
-      <ClassComponent/> 
+ 
+      {/* <ClassComponent/> 
 
-      <FunctionComponent/>        
+      <FunctionComponent/>         */}
+
+      <Props1 num='1' name='홍길동' age='20' score='80'/>
+
+      <Props1 num='2' name='김길동' age='30' score='75'/>
+
+      <Props2 name='박길동' age='25' gender='남'/>
+
+      <Props2 name='박길동' age='25' gender='남'/>
       
-    </>
+      <Props3 name='고길동'/>
+
+      <Props3 name='김영희'/>
+
+
+      <StateEx1/>
+
+      <StateEx2 init="100" step="4"/>
+      </>
   );
 }
 
